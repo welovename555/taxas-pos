5e883af68787f0f8ce770b13fb9fdf1fd07603ae
welovename555 <75744119+welovename555@users.noreply.github.com>
2025-06-18 12:04:06 +0700

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,3 +1,5 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
 const SUPABASE_URL = 'https://nmlcduawkcaaglcdlhgv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tbGNkdWF3a2NhYWdsY2RsaGd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MzM3MDUsImV4cCI6MjA2NTUwOTcwNX0.1NnKZ0begtYSPF7mjDip0lYoR7w1_4qHu5aeDWLWVJY';
 
@@ -5,4 +7,4 @@ if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     console.error("ข้อผิดพลาด: การตั้งค่า Supabase ไม่สมบูรณ์");
 }
 
-export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
